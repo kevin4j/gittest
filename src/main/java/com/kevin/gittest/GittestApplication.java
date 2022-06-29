ef4fe53ede1b8127ec37dab00bc14cd3d63620ed
kevin4j <273613701@qq.com>
2022-06-29 14:33:24 +0800

    v2

--- src/main/java/com/kevin/gittest/GittestApplication.java
+++ src/main/java/com/kevin/gittest/GittestApplication.java
@@ -8,7 +8,7 @@ public class GittestApplication {
 
     public static void main(String[] args) {
         System.out.println("1");
-
+        System.out.println("3");
         System.out.println("2");
         SpringApplication.run(GittestApplication.class, args);
     }
