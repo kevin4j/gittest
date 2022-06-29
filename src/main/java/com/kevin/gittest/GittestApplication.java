cadcac1f1930124c87cf4c25130d8ed94a23a9b1
kevin4j <273613701@qq.com>
2022-06-29 14:41:24 +0800

    v2 (cherry picked from commit 92a370c387442b0530e3df5fc22c317ee730f268)

--- src/main/java/com/kevin/gittest/GittestApplication.java
+++ src/main/java/com/kevin/gittest/GittestApplication.java
@@ -8,10 +8,13 @@ public class GittestApplication {
 
     public static void main(String[] args) {
         System.out.println("1");
-        
+
         System.out.println("2");
-        
+
         System.out.println("4");
+
+        System.out.println("5");
+
         SpringApplication.run(GittestApplication.class, args);
     }
 
